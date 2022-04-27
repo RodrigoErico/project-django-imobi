44c5088acb673447ef9159e8abf00c48294b554b
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-04-26 20:03:17 -0400

    Criado requisições POST no formulário de cadastro.

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        return HttpResponse(f'{username} {email} {password}')
 
 
 def login(request):
