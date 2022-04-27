2db3752dbe12e9e9741ea92fd7ee697d7b98e1da
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-04-27 08:27:10 -0400

    Criado configuração para não aceitar espaços na validação do cadastro.

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
@@ -9,6 +9,10 @@ def cadastro(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
+        
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            return redirect('/auth/cadastro')
+
         return HttpResponse(f'{username} {email} {password}')
 
 
